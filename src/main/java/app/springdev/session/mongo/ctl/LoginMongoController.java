0e5766b0446366ccfa7b46e35170cfca294d9cd5
hippo420 <hippo420@naver.com>
2025-07-28 21:30:32 +0900

    session

--- src/main/java/app/springdev/session/mongo/ctl/LoginMongoController.java
+++ src/main/java/app/springdev/session/mongo/ctl/LoginMongoController.java
@@ -1,4 +1,4 @@
-package app.springdev.session.spring.ctl;
+package app.springdev.session.mongo.ctl;
 
 import app.springdev.session.User;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,14 +12,14 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 @Controller
-@RequestMapping("/session/spring")
-public class LoginSpringController {
+@RequestMapping("/session/mongo")
+public class LoginMongoController {
 
     public static Map<String, String> userSessionMap = new ConcurrentHashMap<>();
 
     @GetMapping("login")
     public String loginView() {
-        return "session/loginspring";
+        return "session/loginmongo";
     }
 
     @PostMapping("prcLogin")
@@ -28,12 +28,12 @@ public class LoginSpringController {
         HttpSession session = request.getSession(true);
         session.setAttribute("userId", user.getUserId());
         session.setAttribute("최근조회상품", "아이폰, 폴드7");
+
         log.info("생성된 세션: {}",session.getId());
         userSessionMap.put(user.getUserId(), session.getId());
-        // Session의 유효 시간 설정 (1800초 = 30분)
         session.setMaxInactiveInterval(1800);
 
-        return "redirect:/session/spring/home?userId="+user.getUserId();
+        return "redirect:/session/mongo/home?userId="+user.getUserId();
     }
 
     @GetMapping("home")
